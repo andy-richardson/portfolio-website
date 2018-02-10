e58d7df5d09adf749b6b215b2a633d9e83302f37
Andy Richardson <andy.john.richardson@gmail.com>
2018-02-10 19:54:40 +0000

    clean up for new site

--- client/includes/javascript/ageCalculator.js
+++
@@ -1,8 +0,0 @@
-const $ = require('jquery');
-
-module.exports = function(selector){
-	const birthday = new Date(1995, 2, 23);
-	const today = new Date();
-	const age = today.getFullYear() - birthday.getFullYear();
-	$(selector).text(age);
-};
--- server/private/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	email : {
-		hostname: 'mail.examplehost.com',
-		user: '"Joe Bloggs" <joe@examplehost.com>'
-	}
-}
