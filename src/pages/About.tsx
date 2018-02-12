7784ae47823de9120e750364bd7f4fdee7e27385
Andy Richardson <andy.john.richardson@gmail.com>
2018-02-12 21:22:32 +0000

    add text and animations to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Icon, Layout, Row } from 'antd';
+import { Button, Col, Icon, Layout, Row, Tag } from 'antd';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -9,9 +9,15 @@ type State = any;
 const FlexContainer = styled.div`
   background: #fff;
   display: flex;
+  padding: 20px 30%;
 
   @media screen and (max-width: 1023px) {
+    padding: 60px;
     flex-direction: column;
+  }
+
+  .slideFromRight {
+    transform: translateX(100vw);
   }
 `;
 
@@ -29,34 +35,85 @@ const FlexItem = styled.div`
 
 const HeaderContainer = FlexItem.extend`
   @media screen and (max-width: 1023px) {
+    flex: auto;
     justify-content: flex-end;
   }
 `;
 
-const ButtonContainer = FlexItem.extend`
-  text-align: center;
-  font-size: 40px;
+const TextContainer = FlexItem.extend`
+`;
+
+const TextItem = styled.p`
+  line-height: 25px;
+  transition: transform 500ms ease-out;
+  transform: translateY(100vh);
+
+  &.animate-in {
+    transform: translateY(0);
+  }
+
+  +p {
+    margin-top: 30px;
+  }
+
+  .ant-tag {
+    margin-right: 0;
+  }
 `;
 
 const HeaderText = styled.h1`
+  transition: transform 500ms ease-out;
+  transform: translateY(100vh);
+
+  &.animate-in {
+    transform: translateY(0);
+  }
 `;
 
 const SubheaderText = styled.h2`
 `;
 
 export default class Hero extends Component<Props, State> {
+  public state: State = {
+    headerIn: false,
+    text1: false,
+    text2: false,
+    text3: false,
+  };
+
+  public componentDidMount() {
+    setTimeout(() => this.setState({headerIn: true}), 200);
+    setTimeout(() => this.setState({text1: true}), 400);
+    setTimeout(() => this.setState({text2: true}), 2000);
+    setTimeout(() => this.setState({text3: true}), 3600);
+  }
+
   public render() {
     return (
       <FlexContainer>
         <HeaderContainer>
-          <HeaderText>About Me</HeaderText>
+          <HeaderText className={(this.state.headerIn) ? 'animate-in' : ''}>
+            Welcome to my world
+          </HeaderText>
         </HeaderContainer>
 
-        <ButtonContainer>
-          <Link to="/about">
-            <Icon type="right-circle-o" />
-          </Link>
-        </ButtonContainer>
+
+        <TextContainer>
+          <TextItem className={(this.state.text1) ? 'animate-in' : ''}>I'm passionate about collaborating with teams to design, architect, and implement
+          great products</TextItem>
+
+          <TextItem className={(this.state.text2) ? 'animate-in' : ''}>
+            I am a strong believer and frequent contributor to open source projects
+          </TextItem>
+
+          <TextItem className={(this.state.text3) ? 'animate-in' : ''}>
+          I have years of experience working with technologies such as
+          &nbsp;<Tag color="blue">React</Tag>
+          &nbsp;<Tag color="magenta">Node</Tag> and
+          &nbsp;<Tag color="green">Docker</Tag>
+          </TextItem>
+
+        </TextContainer>
       </FlexContainer>
     );
   }
